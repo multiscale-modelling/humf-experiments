236322e149fbd7e145286f80430baf7670dfecee
Niklas Kappel <niklas.kappel@kit.edu>
2024-08-21 10:17:07 +0200

    Build node up to gmx

--- src/humf_experiments/nodes/n_h2o_trajectory/__init__.py
+++ src/humf_experiments/nodes/n_h2o_trajectory/__init__.py
@@ -1,6 +1,9 @@
+# pyright: reportAssignmentType=false
+
 import os
 import subprocess
 from pathlib import Path
+from tempfile import TemporaryDirectory
 
 import zntrack as zn
 
@@ -8,13 +11,27 @@ from humf_experiments.nodes.zntrack_utils import zop
 
 
 class NH2OTrajectory(zn.Node):
-    trajectory_gro: str = zn.deps_path()  # pyright: ignore[reportAssignmentType]
-    trajectory_xtc: str = zn.deps_path()  # pyright: ignore[reportAssignmentType]
+    trajectory_gro: str = zn.deps_path()
+    trajectory_xtc: str = zn.deps_path()
+
+    num_molecules: int = zn.params()
+    concatenate: int = zn.params()
 
     def run(self):
         trajectory_gro = Path(self.trajectory_gro)
         trajectory_xtc = Path(self.trajectory_xtc)
         assert trajectory_gro.exists()
         assert trajectory_xtc.exists()
+
         bash_script_path = Path(__file__).parent / "order_and_trj.sh"
-        subprocess.run(["bash", bash_script_path])
+        with TemporaryDirectory() as tempdir:
+            tempdir = Path(tempdir)
+            subprocess.check_output(
+                [
+                    "bash",
+                    bash_script_path,
+                    str(self.num_molecules),
+                    str(self.concatenate),
+                ],
+                cwd=tempdir,
+            )
--- tests/nodes/test_n_h2o_trajectory.py
+++ tests/nodes/test_n_h2o_trajectory.py
@@ -1,6 +1,16 @@
 # pyright: reportCallIssue=false
 
+import shutil
+
+import pytest
+
 from humf_experiments.nodes.n_h2o_trajectory import NH2OTrajectory
+
+
+@pytest.fixture(scope="module", autouse=True)
+def check_gmx_command():
+    if shutil.which("gmx") is None:
+        pytest.skip("Skipping test: 'gmx' command not found.")
 
 
 class TestNH2OTrajectory:
@@ -8,5 +18,7 @@ class TestNH2OTrajectory:
         node = NH2OTrajectory(
             trajectory_gro="tests/inputs/n_h2o_trajectory/trajectory.gro",
             trajectory_xtc="tests/inputs/n_h2o_trajectory/trajectory.xtc",
+            num_molecules=3,
+            concatenate=1,
         )
         node.run()
