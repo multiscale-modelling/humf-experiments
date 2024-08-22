40b7235302b00c8a896d15a8f750af12d9d078d9
is2364 <philipp.dullinger@kit.edu>
2024-08-22 14:51:02 +0200

    runs orca even if False?

---
+++ tests/nodes/test_orca_inp.py
@@ -0,0 +1,23 @@
+# pyright: reportCallIssue=false
+
+import shutil
+
+import pytest
+
+from humf_experiments.nodes.gen_orca_inputs_from_trajectory import convert_trj_to_individual_orca_inputs
+
+
+
+class Test_convert_trj_to_individual_orca_inputs:
+    def test_run(self):
+        node = convert_trj_to_individual_orca_inputs(
+            method_and_basisset=" B3LYP def2-TZVP D3BJ",
+            multiplicity="1",
+            charge="0",
+            pal="1", 
+            run_orca="False",
+            output_dir="tests/outputs/orca_superdir",
+            gro_file="nodes/NH2OTrajectory/n_h2o_trajectory_dir/reduced.gro", # "n_h2o_trajectory_dir/reduced.gro",
+            every_nth_frame=10, 
+)
+        node.run()
