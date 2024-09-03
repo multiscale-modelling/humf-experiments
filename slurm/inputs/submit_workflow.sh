8b26fe4e60a7a275dfcad7bfbb5c215117df6380
Niklas Kappel <niklas.kappel@kit.edu>
2024-09-03 10:49:43 +0200

    Rework workflow scripts

--- slurm/inputs/submit_workflow.sh
+++ slurm/inputs/submit_workflow.sh
@@ -1,5 +1,36 @@
 #!/bin/bash
 
+if ! [ -f pyproject.toml ]; then
+  echo "Error: workflow was not submitted from the project root directory."
+  exit 1
+fi
+
+# <<< git
+if [ -n "$(git status --porcelain)" ]; then
+  echo "Error: git status is not clear."
+  exit 1
+fi
+echo "Current commit hash:"
+git rev-parse HEAD
+# >>>
+
+# <<< poetry
+# shellcheck disable=SC1091
+source "$(poetry env info --path)/bin/activate"
+# >>>
+
+# <<< dvc
+if ! dvc data status -q; then
+  echo "Error: dvc data status is not clear."
+  exit 1
+fi
+if ! dvc status -q; then
+  echo "Error: dvc status is not clear."
+  exit 1
+fi
+python main.py
+# >>>
+
 # Get the directory of the current script
 script_dir=$(dirname "${BASH_SOURCE[0]}")
 
