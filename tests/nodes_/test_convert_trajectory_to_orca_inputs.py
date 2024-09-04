10001b0d861708957db793ece749c176a1955e18
Niklas Kappel <niklas.kappel@kit.edu>
2024-09-04 14:48:12 +0200

    Remove slow Orca test

--- tests/nodes_/test_convert_trajectory_to_orca_inputs.py
+++ tests/nodes_/test_convert_trajectory_to_orca_inputs.py
@@ -2,8 +2,6 @@
 
 import shutil
 
-import pytest
-
 from humf_experiments.nodes.convert_trajectory_to_orca_inputs import (
     ConvertTrajectoryToOrcaInputs,
 )
@@ -23,18 +21,5 @@ class TestConvertTrajectoryToOrcaInputs:
             run_orca=False,
             gro_file="inputs/orca_runfile_generation/reduced.gro",
             every_nth_frame=10,
-        )
-        node.run()
-
-    @pytest.mark.skipif(not orca_is_available(), reason="orca is not available")
-    def test_run_with_orca(self):
-        node = ConvertTrajectoryToOrcaInputs(
-            method_and_basisset="B3LYP def2-TZVP D3BJ",
-            multiplicity="1",
-            charge="0",
-            pal="1",
-            run_orca=True,
-            gro_file="inputs/orca_runfile_generation/reduced.gro",
-            every_nth_frame=100,
         )
         node.run()
