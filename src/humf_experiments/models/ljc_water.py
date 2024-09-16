782a7b2c99af6ac0c86ebaf5383ce8028ed22d9f
Niklas Kappel <niklas.kappel@kit.edu>
2024-09-16 14:29:08 +0200

    Update

--- src/humf_experiments/models/ljc_water.py
+++ src/humf_experiments/models/ljc_water.py
@@ -6,7 +6,7 @@ from humf.layers.interaction_sites.atom_centered_static import AtomCenteredStati
 def create_ljc_water():
     num_atoms_per_mol = 3
 
-    initial_lj_params = np.array([[0.1521, 3.1507], [0.0, 1.0], [0.0, 1.0]])
+    initial_lj_params = np.array([[0.1521, 3.1507], [0.01, 1.0], [0.01, 1.0]])
     lennard_jones_sites = AtomCenteredStatic(
         num_atoms_per_mol=num_atoms_per_mol,
         num_params_per_atom=2,
