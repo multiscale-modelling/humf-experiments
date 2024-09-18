c5735080e63bb81d61c8b37860f902e19833a69f
Niklas Kappel <niklas.kappel@kit.edu>
2024-09-18 14:25:29 +0200

    Add SubmititNode

--- src/humf_experiments/nodes/zntrack_utils.py
+++ src/humf_experiments/nodes/zntrack_utils.py
@@ -1,7 +1,21 @@
 # pyright: reportReturnType=false
 
-import zntrack as zn
+import submitit
+import zntrack
 
 
 def zop(path) -> str:
-    return zn.outs_path(zn.nwd / path)
+    return zntrack.outs_path(zntrack.nwd / path)
+
+
+class SubmititNode(zntrack.Node):
+    def run(self):
+        executor = submitit.AutoExecutor(folder="log_test")
+        executor.update_parameters(**self.get_executor_parameters())
+        executor.submit(self.do_run).result()
+
+    def get_executor_parameters(self):
+        return {}
+
+    def do_run(self):
+        pass
